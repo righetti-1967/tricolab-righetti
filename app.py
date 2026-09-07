bbc8e75dba8ccb7003684dd0ae0182fb07377a6d
Righetti Since 1967 <righetti@righetti.club>
2026-09-07 02:12:48 +0200

    Fix indentation for button in app.py

--- app.py
+++ app.py
@@ -4020,7 +4020,7 @@ def main():
                         use_container_width=True,
                     )
 
-                  if st.button(
+                    if st.button(
                         "💾 Salva Sessione di Analisi",
                         key="btn_salva_analisi_completa",
                         use_container_width=True,
