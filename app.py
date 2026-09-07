7fa5ff66f905feb0db1a61bced69540d46cdb8c3
Righetti Since 1967 <righetti@righetti.club>
2026-09-07 15:45:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3446,7 +3446,7 @@ def main():
                     }
 
                         
-                        col_c1, col_c2, col_c3, col_c4 = st.columns(4)
+                    col_c1, col_c2, col_c3, col_c4 = st.columns(4)
 
                         with col_c1:
                             st.markdown("**Variazione Densità**")
